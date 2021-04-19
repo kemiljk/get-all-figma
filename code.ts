2690eef5f77adbb784819f6547bdf4632fc1876c
Karl Koch <karl@kejk.tech>
2021-04-19 15:19:12 +0100

    init

---
+++ code.ts
@@ -0,0 +1,23 @@
+figma.showUI(__html__, { width: 300, height: 120 });
+
+figma.ui.onmessage = (msg) => {
+  if (msg.type === "get-instances") {
+    let iconName: String;
+    let getInstances;
+    const { selection } = figma.currentPage;
+
+    function getInstanceByName() {
+      iconName = selection[0].name;
+    }
+
+    getInstanceByName();
+
+    getInstances = figma.currentPage
+      .findAll()
+      .filter((node) => node.name === iconName);
+    figma.currentPage.selection = getInstances;
+    figma.notify(`${getInstances.length} "${iconName}'s" selected`);
+  }
+
+  figma.closePlugin();
+};
